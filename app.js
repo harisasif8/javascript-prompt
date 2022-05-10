500f7f9184a732cbda7579ac50c5e138a026b022
harisasif8 <harisasif222@gmail.com>
2022-05-10 17:58:25 +0500

    prompt using 1 id

--- app.js
+++ app.js
@@ -19,6 +19,28 @@
 // f.innerHTML= c
 
 
+// const arr = ['name', 'company', 'designation' , 'salary' ]
+// const arr2 = [];
+// for (let i = 0; i<arr.length; i++ ){
+//     const a = prompt('enter your ' + arr[i])
+//     arr2.push(a)
+    
+    
+// }
+
+// let d = document.getElementById('name')
+//     d.innerHTML= arr2[0]
+    
+//     let e = document.getElementById('company')
+//     e.innerHTML= arr2[1]
+    
+//     let f = document.getElementById('designation')
+//     f.innerHTML= arr2[2]
+
+//     let g = document.getElementById('salary')
+//     g.innerHTML= arr2[3]
+
+
 const arr = ['name', 'company', 'designation' , 'salary' ]
 const arr2 = [];
 for (let i = 0; i<arr.length; i++ ){
@@ -28,23 +50,20 @@ for (let i = 0; i<arr.length; i++ ){
     
 }
 
-let d = document.getElementById('name')
-    d.innerHTML= arr2[0]
-    asd
-    let e = document.getElementById('company')
-    e.innerHTML= arr2[1]
-    
-    let f = document.getElementById('designation')
-    f.innerHTML= arr2[2]
 
-    let g = document.getElementById('salary')
-    g.innerHTML= arr2[3]
+let d = document.getElementById('list')
+
+for (let j = 0; j<arr2.length; j++ ){
 
 
+    const li = document.createElement('li');
+const liText = document.createTextNode(arr2[j])
+li.appendChild(liText)
 
+d.appendChild(li)
 
-// let d = document.getElementById('name')
-//     d.innerHTML= arr2[0]
+}
+
 
 
 
